aa25d8459fa00ce5eec6af05420ccd6c4451eeed
esjones10 <esjones10@crimson.ua.edu>
2026-03-05 12:06:33 -0600

    Added dynamic variables for the email templete and created testing preview

--- src/apps/core/email.py
+++ src/apps/core/email.py
@@ -1,3 +1,5 @@
+import os
+from django.conf import settings
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.utils import timezone
@@ -8,3 +10,24 @@ def display_login_email(user_id):
 
     formatted_time = credential.expires_at.strftime("%m/%d/%Y at %I:%M %p")
 
+# Used for testing the view of the email templete
+def generate_preview_file():
+    """
+    Generates a static HTML file in src/static/email_preview.html 
+    populated with dummy data so you can view the design in a browser.
+    """
+    context = {
+        'name': 'Valued Client',
+        'expiration_time': timezone.now().strftime("%m/%d/%Y at %I:%M %p"),
+        'login_id': 'INT-PREVIEW-123',
+        'password': 'sample-password-123'
+    }
+    
+    html_content = render_to_string('email_temp.html', context)
+    
+    output_path = os.path.join(settings.BASE_DIR, 'src', 'static', 'email_preview.html')
+    
+    with open(output_path, 'w') as f:
+        f.write(html_content)
+    
+    return f"Preview generated at: {output_path}"
